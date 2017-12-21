fd82b2f4960f46ccdc62216d8c22442108f3fcd1
Peter Jang <peter.x.jang@gmail.com>
2017-12-21 00:12:35 -0600

    Add post model with RESTful API routes

---
+++ app/controllers/v1/posts_controller.rb
@@ -0,0 +1,43 @@
+class V1::PostsController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+
+  def index
+    posts = Post.all
+    render json: posts.as_json
+  end
+
+  def create
+    post = Post.new(
+      title: params[:title],
+      body: params[:body],
+      user_id: current_user.id
+    )
+    if post.save
+      render json: post.as_json
+    else
+      render json: {errors: post.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def show
+    post = Post.find_by(id: params[:id])
+    render json: post.as_json
+  end
+
+  def update
+    post = Post.find_by(id: params[:id])
+    post.title = params[:title] || post.title
+    post.body = params[:body] || post.body
+    if post.save
+      render json: post.as_json
+    else
+      render json: {errors: post.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def destroy
+    post = Post.find_by(id: params[:id])
+    post.destroy
+    render json: {message: "Post successfully destroyed!"}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   namespace :v1 do
     post "/users" => "users#create"
+
+    get "/posts" => "posts#index"
+    post "/posts" => "posts#create"
+    get "/posts/:id" => "posts#show"
+    patch "/posts/:id" => "posts#update"
+    delete "/posts/:id" => "posts#destroy"
   end
 end
